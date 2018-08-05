ce7c3300fe3a266ef1c113cd0a63961b95b8ac16
Angga Ari Wijaya <anggadarkprince@gmail.com>
2018-08-05 11:32:39 +0700

    create robofriend with react

---
+++ src/components/CardList.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Card from "./Card";
+
+const CardList = ({robots}) => {
+    return (
+        <div>
+            {
+                robots.map((robot, index) => {
+                    return <Card key={index} id={robot.id} name={robot.name} email={robot.email}/>
+                })
+            }
+        </div>
+    )
+}
+
+export default CardList;
