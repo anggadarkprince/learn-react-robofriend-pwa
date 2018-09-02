32ace1dcc687e6cb97d021ea9ebc8f0c5b5dc15e
Angga Ari Wijaya <anggadarkprince@gmail.com>
2018-09-02 10:09:28 +0700

    optimize with pwa specification by lighthouse recommendation

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,7 +3,9 @@ import React from 'react';
 const SearchBox = ({searchChange}) => {
     return (
         <div className='pa2'>
-            <input className='pa3 ba b--green bg-lightest-blue' type='search' placeholder='search robots' onChange={searchChange}/>
+            <input aria-label='Search robot'
+                className='pa3 ba b--green bg-lightest-blue'
+                   type='search' placeholder='search robots' onChange={searchChange}/>
         </div>
     )
 }
