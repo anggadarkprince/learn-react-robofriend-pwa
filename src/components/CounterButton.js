bd88de0a5827e4ac8d3d0bdcfb835eab92ef1bc7
Angga Ari Wijaya <anggadarkprince@gmail.com>
2018-11-24 21:13:32 +0700

    add prettierrc config

--- src/components/CounterButton.js
+++ src/components/CounterButton.js
@@ -1,33 +1,33 @@
 import React, { Component } from "react";
 
 class CounterButton extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      count: 0
-    };
-  }
-
-  shouldComponentUpdate(nextProps, nextState) {
-    if (this.state.count !== nextState) {
-      return true;
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 0,
+        };
     }
-    return false;
-  }
 
-  updateCount = () => {
-    this.setState(state => {
-      return { count: state.count + 1 };
-    });
-  };
+    shouldComponentUpdate(nextProps, nextState) {
+        if (this.state.count !== nextState) {
+            return true;
+        }
+        return false;
+    }
 
-  render() {
-    return (
-      <button color={this.props.color} onClick={this.updateCount}>
-        Count: {this.state.count}
-      </button>
-    );
-  }
+    updateCount = () => {
+        this.setState(state => {
+            return { count: state.count + 1 };
+        });
+    };
+
+    render() {
+        return (
+            <button color={this.props.color} onClick={this.updateCount}>
+                Count: {this.state.count}
+            </button>
+        );
+    }
 }
 
 export default CounterButton;
