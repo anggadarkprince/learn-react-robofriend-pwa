421bc648479f799aa2603be6f363fdd3e93db647
Angga Ari Wijaya <anggadarkprince@gmail.com>
2018-11-24 20:47:45 +0700

    Updates

---
+++ precache-manifest.08c65f5c73e9d04f69dc9e647bea00b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/learn-react-robofriend-pwa/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "97fb5191c163739b4e5d",
+    "url": "/learn-react-robofriend-pwa/static/js/runtime~main.97fb5191.js"
+  },
+  {
+    "revision": "d9e85cedc78c7276cf8f",
+    "url": "/learn-react-robofriend-pwa/static/js/main.d9e85ced.chunk.js"
+  },
+  {
+    "revision": "dc9601e32d431055747d",
+    "url": "/learn-react-robofriend-pwa/static/js/1.dc9601e3.chunk.js"
+  },
+  {
+    "revision": "d9e85cedc78c7276cf8f",
+    "url": "/learn-react-robofriend-pwa/static/css/main.7fa268d8.chunk.css"
+  },
+  {
+    "revision": "dc9601e32d431055747d",
+    "url": "/learn-react-robofriend-pwa/static/css/1.24a7c7fc.chunk.css"
+  },
+  {
+    "revision": "c37ce82db625b88973a6b0de29cd2aa8",
+    "url": "/learn-react-robofriend-pwa/index.html"
+  }
+];
