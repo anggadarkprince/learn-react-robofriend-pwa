a089681b506921206bb3935f53826649562b0d8a
Angga Ari Wijaya <anggadarkprince@gmail.com>
2018-11-24 21:07:17 +0700

    add prettier hook

--- src/components/CounterButton.js
+++ src/components/CounterButton.js
@@ -1,33 +1,33 @@
-import React, {Component} from 'react'
+import React, { Component } from "react";
 
 class CounterButton extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            count: 0
-        }
-    }
-
-    shouldComponentUpdate(nextProps, nextState) {
-        if(this.state.count !== nextState) {
-            return true;
-        }
-        return false;
-    }
-
-    updateCount = () => {
-        this.setState(state => {
-            return {count: state.count + 1}
-        })
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
     };
+  }
 
-    render() {
-        return (
-            <button color={this.props.color} onClick={this.updateCount}>
-                Count: {this.state.count}
-            </button>
-        )
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.state.count !== nextState) {
+      return true;
     }
+    return false;
+  }
+
+  updateCount = () => {
+    this.setState(state => {
+      return { count: state.count + 1 };
+    });
+  };
+
+  render() {
+    return (
+      <button color={this.props.color} onClick={this.updateCount}>
+        Count: {this.state.count}
+      </button>
+    );
+  }
 }
 
 export default CounterButton;
