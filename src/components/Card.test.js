fac45009306400f7b1c583366bc6d5f83f807558
Angga Ari Wijaya <anggadarkprince@gmail.com>
2018-11-24 20:09:36 +0700

    update circleci config

--- src/components/Card.test.js
+++ src/components/Card.test.js
@@ -3,15 +3,6 @@ import React from 'react';
 
 import Card from "./Card";
 
-describe('MyComponent', () => {
-    it('should render correctly in "debug" mode', () => {
-        const component = shallow(<Card debug />);
-        expect(component).toMatchSnapshot();
-    });
-});
-
-/*
 it('expect to render Card component', () => {
     expect(shallow(<Card/>)).toMatchSnapshot();
 });
-*/
